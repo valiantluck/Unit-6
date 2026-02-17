32131724c6bb366d67569f05d425cac0231f50f7
Fee <felicity124martin@gmail.com>
2026-02-17 10:25:14 -0500

    using values with dictionaries

--- alien.py
+++ alien.py
@@ -1,11 +1,19 @@
 
-alien_0 = {1: "green", "points": 5}
-print(alien_0[1])
-print(alien_0["points"])
-
-modifier = .2
-
-new_points = alien_0["points"]
-new_points = new_points * (1 + modifier)
-print(f"You just earned {new_points} points!")
+alien_0 = {}
 print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+
+print(alien_0)
+
+alien_0['color'] = 'yellow'
+
+print(alien_0)
+
