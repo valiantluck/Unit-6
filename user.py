2f6b9086fcab54093b5020c039b4ac81dd3c3708
Fee <felicity124martin@gmail.com>
2026-02-17 11:51:28 -0500

    update user.py

--- user.py
+++ user.py
@@ -5,6 +5,6 @@ user_0 ={
 }
 
 for key, value in user_0.items(): # items() method returns a list of key-value pairs in a dictionary
-    print(f"\nKey: {k}") # key is the variable that holds the key in the dictionary
-    print(f"Value: {v}") # value is the variable that holds the value associated with the key in the dictionary
+    print(f"\nKey: {key}") # key is the variable that holds the key in the dictionary
+    print(f"Value: {value}") # value is the variable that holds the value associated with the key in the dictionary
     
