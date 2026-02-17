41cc2f4f18d3de6697a1290410b707e00cb9880e
Fee <felicity124martin@gmail.com>
2026-02-17 11:12:33 -0500

    updated

--- fav_lang.py
+++ fav_lang.py
@@ -4,5 +4,6 @@ fav_lang = {'jen': 'python', 'sarah': 'c',
 # using values 
 print("The following languages have been mentioned:")
 
-for language in fav_lang.values():
+for language in set(fav_lang.values()): 
     print(language.title())
+#set() function removes duplicates from the list of values
